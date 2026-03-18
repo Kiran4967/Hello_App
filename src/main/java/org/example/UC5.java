954c0bb5b7dd7c8458fa007c4dd2718e42a9ea6c
Kiran4967 <kirantejaprakashofficial@gmail.com>
2026-03-18 14:55:36 +0530

    UC5

---
+++ src/main/java/org/example/UC5.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class UC5 {
+
+    public static void main(String[] args) {
+
+        // If no arguments are provided
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        }
+        else {
+            // Build a comma-separated list of names
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
+
+    }
+}
