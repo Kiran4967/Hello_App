a69c0504af7d767dd71e9f2ad75c24b1b966c890
Kiran4967 <kirantejaprakashofficial@gmail.com>
2026-03-26 15:17:48 +0530

    UC4

--- src/main/java/org/example/UC4.java
+++ src/main/java/org/example/UC4.java
@@ -1,24 +1,17 @@
 package org.example;
 
 public class UC4 {
-
     public static void main(String[] args) {
-
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-
-            StringBuilder names = new StringBuilder();
-
-            for (int i = 0; i < args.length; i++) {
-                names.append(args[i]);
-
-                if (i < args.length - 1) {
-                    names.append(", ");
-                }
-            }
-
-            System.out.println("Hello, " + names.toString() + "!");
-        }
+        String name="";
+        StringBuilder nameBuilder = new StringBuilder();
+        for(int i=0;i<args.length;i++){
+            nameBuilder.append(args[i]);
+            if(i <args.length - 1) {
+                nameBuilder.append(", ");
+                }}
+                name = nameBuilder.toString();
+                System.out.print(name);
     }
-}
+
+    }
+
